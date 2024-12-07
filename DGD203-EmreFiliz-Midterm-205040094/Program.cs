92ae1d96ca8896a2fa6cf41231e9eecfc5a81536
emrefiliz01 <98563077+emrefiliz01@users.noreply.github.com>
2024-12-07 16:36:02 +0300

    asking name added

--- DGD203-EmreFiliz-Midterm-205040094/Program.cs
+++ DGD203-EmreFiliz-Midterm-205040094/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SpaceQuest
+{
+    class SpaceAdventure
+    {
+        private string playerName;
+        public void Start()
+        {
+            AskPlayerName();
+            GreetPlayer();
+            WaitForNextStep();
+        }
+
+        private void AskPlayerName()
+        {
+            Console.WriteLine("Greetings, space traveler! What's your name?");
+            playerName = Console.ReadLine();
+        }
+
+        private void GreetPlayer()
+        {
+            Console.WriteLine($"Welcome, Captain {playerName}! Prepare for your journey across the stars.");
+        }
+
+        private void WaitForNextStep()
+        {
+            Console.WriteLine("Press any key to launch...");
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpaceAdventure game = new SpaceAdventure();
+            game.Start();
+        }
+    }
+}
